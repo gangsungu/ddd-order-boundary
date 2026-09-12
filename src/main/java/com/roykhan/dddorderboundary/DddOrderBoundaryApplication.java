23ffb950a9b9e2b62906bcd4e48814337cd00639
gangsungu <ark_@naver.com>
2026-09-12 20:33:56 +0900

    chore: 프로젝트 초기 설정
    - Spring Boot 4.1.1 / Java 21 / Gradle

---
+++ src/main/java/com/roykhan/dddorderboundary/DddOrderBoundaryApplication.java
@@ -0,0 +1,13 @@
+package com.roykhan.dddorderboundary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddOrderBoundaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddOrderBoundaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roykhan/dddorderboundary/DddOrderBoundaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roykhan.dddorderboundary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddOrderBoundaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
