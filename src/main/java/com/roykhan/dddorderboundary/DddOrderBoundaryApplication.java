969ab22b7254bdc6b9889b8632884c2275a8bdb6
gangsungu <ark_@naver.com>
2026-09-19 14:47:33 +0900

    refactor: JPA Auditing 설정을 메인 클래스에서 분리

--- src/main/java/com/roykhan/dddorderboundary/DddOrderBoundaryApplication.java
+++ src/main/java/com/roykhan/dddorderboundary/DddOrderBoundaryApplication.java
@@ -2,10 +2,8 @@ package com.roykhan.dddorderboundary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DddOrderBoundaryApplication {
 
     public static void main(String[] args) {
